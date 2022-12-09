3771f04d36604eeac27b3d0e74caac85765d1c8b
LP <lp.passarelli.wilson@gmail.com>
2022-12-09 10:48:20 -0800

    added in journal.js and changed the opening page

---
+++ src/components/Journal.js
@@ -0,0 +1,21 @@
+//this sets up Journal as a component.
+function Journal() {
+  const myJournal = {
+    id: 1,
+    design: "Lord of the Rings",
+    sub_design: "Rohan",
+    dye: "Bison Brown",
+  };
+  return (
+    //specify anything you want to be rendered to the user
+    <div>
+      <h2>{myJournal.design}</h2>
+      <ul>
+        <li>{myJournal.sub_design}</li>
+        <li>{myJournal.dye}</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Journal;
